a005bba5fd3b5da6575ce82d38357cb6a3dc2f11
Rishi-314 <rishikesh2048x@gmail.com>
2026-09-10 22:26:54 +0530

    Spring starter

---
+++ ZDT-HS/src/main/java/com/EDI/ZDT_HS/ZdtHsApplication.java
@@ -0,0 +1,13 @@
+package com.EDI.ZDT_HS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZdtHsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZdtHsApplication.class, args);
+	}
+
+}
---
+++ ZDT-HS/src/test/java/com/EDI/ZDT_HS/ZdtHsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EDI.ZDT_HS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZdtHsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
